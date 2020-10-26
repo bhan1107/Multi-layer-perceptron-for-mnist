097ccef8efe8a97d1144f58e5ac93c278081f8df
bhan1107 <bhan1107@gmail.com>
2020-10-26 17:07:56 -0400

    Working MLP with Back propagation
    Removed data files

--- Assignment 2/Tensorflow_MLP.py
+++ Assignment 2/Tensorflow_MLP.py
@@ -1,12 +1,10 @@
 
-import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from operator import truediv
 
 class Keras_MLP(object):
     def __init__(self):
-        mnist = keras.datasets.mnist
+        mnist = keras.datasets.mnist # input data
         (X_train,Y_train),(X_test,Y_test) = mnist.load_data()
         self.trainInput  = np.asarray(X_train).astype(np.float32)
         self.trainOutput = np.asarray(Y_train).astype(np.int)
@@ -14,52 +12,30 @@ class Keras_MLP(object):
         self.testInput  = np.asarray(X_test).astype(np.float32)
         self.testOutput = np.asarray(Y_test).astype(np.int)
         self.testInput  = self.testInput / 255 # Normalize data
-    def createNN(self):
+
+    def createNN(self, function_name, hidden_size, output_size, learning_rate, momentum_rate): # Creating neural network with parameters
         self.model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
-        keras.layers.Dense(64, activation='sigmoid'),
-        keras.layers.Dense(10, activation='sigmoid') ])
+        keras.layers.Dense(hidden_size, activation=function_name), # hidden node size and which activation functions to use
+        keras.layers.Dense(output_size, activation=function_name) ]) # output node size and which activation functions to use
 
-        #custom_optimizer = keras.optimizers.Adam(lr=0.01)
-        custom_optimizer = keras.optimizers.SGD(lr=0.09, momentum = 0.11)
-        self.model.compile(optimizer=custom_optimizer,
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy'])
+        custom_optimizer = keras.optimizers.SGD(lr=learning_rate, momentum = momentum_rate) # learning rate and momentum rate
+        self.model.compile(optimizer=custom_optimizer,loss='sparse_categorical_crossentropy',metrics=['accuracy']) # Compile neural network
 
-    def trainNN(self):
-        self.model.fit(self.trainInput, self.trainOutput, epochs = 10)
-        pred = self.model.predict(self.trainInput)
-        con = tf.math.confusion_matrix(self.trainOutput, np.argmax(pred,axis=1))
-        print('Test : ',con)
-        np.savetxt("train.csv", con, delimiter=",", fmt='%1.0f')
-        tp = np.diag(con)
-        prec = list(map(truediv, tp, np.sum(con, axis=0)))
-        rec = list(map(truediv, tp, np.sum(con, axis=1)))
-        print('Precision : ',np.around(np.asarray(prec),4))
-        print('Recall    : ',np.around(np.asarray(rec),4))
-        print('Precision avg : ',np.sum(np.asarray(prec) / 10))
-        print('Recall    avg : ',np.sum(np.asarray(rec) / 10))
+    def trainNN(self, epoch):
+        self.model.fit(self.trainInput, self.trainOutput, epochs = epoch) # Repeat train epoch amout 
 
-
-
-    def testNN(self):
-        test_loss, test_acc = self.model.evaluate(self.testInput, self.testOutput, verbose=2)
-        print('Test Result : ', test_acc)
-        pred = self.model.predict(self.testInput)
-        con = tf.math.confusion_matrix(self.testOutput, np.argmax(pred,axis=1))
-        print(con)
-        np.savetxt("test.csv", con, delimiter=",", fmt='%1.0f')
-        tp = np.diag(con)
-        prec = list(map(truediv, tp, np.sum(con, axis=0)))
-        rec = list(map(truediv, tp, np.sum(con, axis=1)))
-        print('Precision : ',np.around(np.asarray(prec),4))
-        print('Recall    : ',np.around(np.asarray(rec),4))
-        print('Precision avg : ',np.sum(np.asarray(prec) / 10))
-        print('Recall    avg : ',np.sum(np.asarray(rec) / 10))
-
+    def testNN(self, model_name):
+        test_loss, test_acc = self.model.evaluate(self.testInput, self.testOutput, verbose=2) # Evaluate neural network's performance
+        print(model_name , '- Test Result : ', np.around(test_acc*100, 2), '%') # Print neural network's accuracy
 
 if __name__ == "__main__":
-    kp = Keras_MLP()
-    kp.createNN()
-    kp.trainNN()
-    kp.testNN()
+    nn = Keras_MLP() # Model 2A
+    nn.createNN('sigmoid', 64, 10, 0.07, 0.11)
+    nn.trainNN(5)
+    nn.testNN('Model 2A')
+
+    kp = Keras_MLP() # Model 2B
+    kp.createNN('sigmoid', 100, 10, 0.09, 0.11)
+    kp.trainNN(10)
+    kp.testNN('Model 2B')
--- Assignment 2/TheMLP.py
+++ Assignment 2/TheMLP.py
@@ -1,10 +1,9 @@
-import numpy as np
 from tensorflow.keras.datasets import mnist
-import tensorflow as tf
+import numpy as np
 from operator import truediv
 
 class Neural_Network(object):
-    def __init__(self, learningRate, inputSize, hiddenSize, outputSize, momentum):
+    def __init__(self, learningRate, inputSize, hiddenSize, outputSize, momentum): # Initialize neural network
         # Set parameters
         self.inputSize = inputSize
         self.hiddenSize = hiddenSize
@@ -23,63 +22,67 @@ class Neural_Network(object):
         self.incorrect = 0
         self.train_outputs = [] # For confusion matrix
         self.test_outputs = [] # For confusion matrix
+        self.MSE = 1
 
-    def sigmoidFunction(self, value):
+    def sigmoidFunction(self, value): # sigmoid function
         return 1/(1 + np.exp(-value))
     
-    def feedForward(self,input, desired):
+    def feedForward(self,input, desired): # For feedforward, calculation of output
         self.hiddenLayerNet = np.dot(input,self.inputToHiddenWeight) + self.biasWeightih # wixi + b for input to hidden layer
         self.hiddenLayerOut = np.asarray([self.sigmoidFunction(neuron) for neuron in self.hiddenLayerNet]) # sigmoid(wixi + b) for input to hidden
-        self.outputLayerNet = np.dot(self.hiddenLayerOut, self.hiddenToOutputWeight) + self.biasWeightho
-        self.outputLayerOut = np.asarray([self.sigmoidFunction(neuron) for neuron in self.outputLayerNet])
+        self.outputLayerNet = np.dot(self.hiddenLayerOut, self.hiddenToOutputWeight) + self.biasWeightho # wixi + b for hidden to output layer
+        self.outputLayerOut = np.asarray([self.sigmoidFunction(neuron) for neuron in self.outputLayerNet]) # sigmoid(wixi + b) for hidden to output layer
 
         self.hiddenLayerOut = self.hiddenLayerOut.reshape(1,64)
         self.train_outputs.append(np.argmax(self.outputLayerOut)) # For confusion matrix 
 
-        if(desired != None):
+        if(desired != None): # Performance check
             self.test_outputs.append(np.argmax(self.outputLayerOut))
             if(np.argmax(self.outputLayerOut) == desired): # If classification is correct += 1
                 self.correct += 1
             else:
                 self.incorrect += 1
     
-    def backPropagation(self, inputLayer, desired):
+    def backPropagation(self, inputLayer, desired): # For back propagation learnign
         self.desired_output = np.zeros(10)
-        self.desired_output[desired] = 1.0 # Represents desired output in one-hot encoding format [0,1,0,0,0,0,0]
+        self.desired_output[desired] = 1.0 # Represents desired output in one-hot encoding format [0,1,0,0,0,0,0,0,0,0]
 
         output_error = ( self.outputLayerOut - self.desired_output ) * self.outputLayerOut * ( 1 - self.outputLayerOut ).reshape(1,10) # Calculation of output layer delta
         self.hiddenToOutputWeight = self.hiddenToOutputWeight - ( np.dot(self.hiddenLayerOut.T,output_error) * self.learningRate + ( self.weightDelta * self.momentum ) ) # calculation of change in hiden layer to output layer weight + moementum
         self.biasWeightho -= self.learningRate * output_error # Update on bias weight
 
-        hidden_error = ( np.dot(output_error,self.hiddenToOutputWeight.T) * self.hiddenLayerOut * (1 - self.hiddenLayerOut) ).reshape(1,64)
+        hidden_error = ( np.dot(output_error,self.hiddenToOutputWeight.T) * self.hiddenLayerOut * (1 - self.hiddenLayerOut) ).reshape(1,64) # Calculation of hidden layer delta
         inputLayer = inputLayer.reshape(1,784)
-        self.inputToHiddenWeight = self.inputToHiddenWeight - ( self.learningRate * np.dot(inputLayer.T, hidden_error) + ( self.weightDelta2 * self.momentum ) )
-        self.biasWeightih -= self.learningRate * hidden_error
+        self.inputToHiddenWeight = self.inputToHiddenWeight - ( self.learningRate * np.dot(inputLayer.T, hidden_error) + ( self.weightDelta2 * self.momentum ) ) # calculation of change in input layer to hidden layer weight + moementum
+        self.biasWeightih -= self.learningRate * hidden_error # Update on bias weight
 
         self.weightDelta  = output_error # For momentum
         self.weightDelta2 = hidden_error # For momentum
 
     
     def Mean_Squared_Error(self,d,y): # For MSE calculation
-        return np.mean((d - y)**2)
+        self.MSE = np.mean((d - y)**2)
+        return self.MSE
     
-    def train(self, epoch, X_train,Y_train):
+    def train(self, epoch, X_train,Y_train): # Begin training
         trainInput  = np.asarray(X_train).astype(np.float32)
         trainOutput = np.asarray(Y_train).astype(np.int)
         trainInput  = trainInput / 255 # Normalize data
 
-        for num in range(epoch):
+        for num in range(epoch) : # Runs for this many epoch
             self.train_outputs = [] # For confusion matrix
             self.test_outputs = [] # For confusion matrix
-            for i in range(len(trainInput)):
+            for i in range(len(trainInput)): # Run training length of input data for mnist, 60000 times
                 nn.feedForward(trainInput[i].flatten(), None)
                 nn.backPropagation(np.asarray(trainInput[i].flatten()),trainOutput[i])
 
                 if i%400 == 0:
-                    print('MSE : ',self.Mean_Squared_Error(self.desired_output, self.outputLayerOut))
-    
-    def test(self, X_test,Y_test):
+                    if self.MSE < 0.0000005 : # If MSE is lower than threshold then stop trraining
+                        break
+                    print('MSE : ',self.Mean_Squared_Error(self.desired_output, self.outputLayerOut)) # Print MSE
 
+     
+    def test(self, X_test,Y_test): # Test neural network's performance
         testInput  = np.asarray(X_test).astype(np.float32)
         testOutput = np.asarray(Y_test).astype(np.int)
         testInput  = testInput / 255 # Normalize data
@@ -87,13 +90,20 @@ class Neural_Network(object):
         for i in range(len(testInput)):
             nn.feedForward(testInput[i].flatten(), testOutput[i])
 
-        print('Correct : ', nn.correct)
+        print('Correct   : ', nn.correct)
         print('Incorrect : ', nn.incorrect)
-        print('Percentage ', (nn.correct / len(testInput)) * 100, '%')
+        print('Accuracy  : ', (nn.correct / len(testInput)) * 100, '%')
     
 if __name__ == "__main__":
-    (X_train,Y_train),(X_test,Y_test) = mnist.load_data()
+    # Without printing confusion matrix
+    (X_train,Y_train),(X_test,Y_test) = mnist.load_data() # Load data
+    nn = Neural_Network(0.07,784,64,10, 0.11) # Initialize with learning rate, input layer size, hidden layer size, output layer size and momentum value
+    nn.train(5,X_train,Y_train) # Train neural network with given number of epoch
+    nn.test(X_test,Y_test) # Test and print result of test data set
 
+
+    # With printing confusion matrix
+    '''(X_train,Y_train),(X_test,Y_test) = mnist.load_data()
     nn = Neural_Network(0.07,784,64,10, 0.11)
     nn.train(5,X_train,Y_train)
     con = tf.math.confusion_matrix(Y_train, nn.train_outputs)
@@ -102,11 +112,10 @@ if __name__ == "__main__":
     tp = np.diag(con)
     prec = list(map(truediv, tp, np.sum(con, axis=0)))
     rec = list(map(truediv, tp, np.sum(con, axis=1)))
-    print('Precision : ',np.around(np.asarray(prec),4))
-    print('Recall    : ',np.around(np.asarray(rec),4))
-    print('Precision avg : ',np.sum(np.asarray(prec) / 10))
-    print('Recall    avg : ',np.sum(np.asarray(rec) / 10))
-    #print ('Precision: {}\nRecall: {}'.format(prec, rec))
+    print('Precision       : ',np.around(np.asarray(prec),4))
+    print('Recall          : ',np.around(np.asarray(rec),4))
+    print('Total Precision : ',np.sum(np.asarray(prec) / 10))
+    print('Total Recall    : ',np.sum(np.asarray(rec) / 10))
 
     nn.test(X_test,Y_test)
     con = tf.math.confusion_matrix(Y_test, nn.test_outputs)
@@ -118,5 +127,4 @@ if __name__ == "__main__":
     print('Precision : ',np.around(np.asarray(prec),4))
     print('Recall    : ',np.around(np.asarray(rec),4))
     print('Precision avg : ',np.sum(np.asarray(prec) / 10))
-    print('Recall    avg : ',np.sum(np.asarray(rec) / 10))
-    #print ('Precision: {}\nRecall: {}'.format(prec, rec))
+    print('Recall    avg : ',np.sum(np.asarray(rec) / 10))'''
